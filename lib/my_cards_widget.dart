2862409970a3e4c5d06df383d5b1d685de5b6d42
Moe <Mohammed27Zalzala11@gmail.com>
2022-11-07 18:10:26 +0300

    create virtual card button created

--- lib/my_cards_widget.dart
+++ lib/my_cards_widget.dart
@@ -6,16 +6,50 @@ class MyCardsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.redAccent,
-        child: const Center(
-            child: Text(
-          'Cards',
-          style: TextStyle(color: Colors.white70),
-        )),
+      backgroundColor: const Color(0xff1C1C1C),
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {},
+          child: const Icon(
+            Icons.menu,
+            color: Color(0xFFD9D9D9),
+          ),
+        ),
+        title: const Text(
+          'My Cards',
+          style: TextStyle(color: Color(0xFFD9D9D9)),
+        ),
+        backgroundColor: Colors.black54,
+      ),
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: const Color(0xFF1C1C1C),
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 118,
+                padding: const EdgeInsets.all(26),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF343434),
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Color(0xFFDFDFDF)),
+                          borderRadius: BorderRadius.circular(20)),
+                  ),
+                  child: const Text('Create virtual card',style: TextStyle(
+                    color: Color(0xFFDFDFDF),
+                    fontSize: 24,
+                    fontWeight: FontWeight.normal
+                  ),),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
