d2b45f037ae4f71e7f991f27c2065c2079ba09df
Moe <Mohammed27Zalzala11@gmail.com>
2022-11-08 22:16:10 +0300

    Card widget created

--- lib/my_cards_widget.dart
+++ lib/my_cards_widget.dart
@@ -36,17 +36,74 @@ class MyCardsWidget extends StatelessWidget {
                   onPressed: () {},
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color(0xFF343434),
-                      shape: RoundedRectangleBorder(
+                    shape: RoundedRectangleBorder(
                         side: const BorderSide(color: Color(0xFFDFDFDF)),
-                          borderRadius: BorderRadius.circular(20)),
+                        borderRadius: BorderRadius.circular(20)),
                   ),
-                  child: const Text('Create virtual card',style: TextStyle(
-                    color: Color(0xFFDFDFDF),
-                    fontSize: 24,
-                    fontWeight: FontWeight.normal
-                  ),),
+                  child: const Text(
+                    'Create virtual card',
+                    style: TextStyle(
+                        color: Color(0xFFDFDFDF),
+                        fontSize: 24,
+                        fontWeight: FontWeight.normal),
+                  ),
                 ),
               ),
+              Expanded(
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  itemExtent: 120,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      color: const Color(0XFF343434),
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Card User Name',
+                            style: TextStyle(
+                                fontSize: 20, color: Color(0xFFDFDFDF)),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Card Number',
+                                style: TextStyle(
+                                    fontSize: 14, color: const Color(0xFFDFDFDF).withOpacity(0.8)),
+                              ),
+                              const Icon(
+                                Icons.arrow_forward_ios,
+                                color: Color(0XFFDFDFDF),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Card Date',
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: const Color(0XFFDFDFDF)
+                                        .withOpacity(0.5)),
+                              ),
+                              Text('Balance',
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: const Color(0XFFDFDFDF)
+                                          .withOpacity(0.5))),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
